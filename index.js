61c1662acf366ae01a724dbdc6bdddc632184a42
jpiresl <Juan.PiresL21@gmail.com>
2020-12-07 13:52:52 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+
+
+
+const port = 3333;
+app.listen(port, console.log(`Server on na porta: ${port}`))
